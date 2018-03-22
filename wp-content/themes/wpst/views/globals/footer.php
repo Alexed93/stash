2184a7a3896fe5570b3b3800309964ca9791f60c
Alex Edwards <alexed93@gmail.com>
2018-03-22 22:25:58 +0000

    Finalised header

--- wp-content/themes/wpst/views/globals/footer.php
+++ wp-content/themes/wpst/views/globals/footer.php
@@ -14,11 +14,37 @@
 
 ?>
 
-<footer>
+
+<footer class="footer">
     <div class="container">
-        Footer
+
+        <a href="/" class="logo icon icon--logo icon--xlarge | footer__logo">
+            <span class="is-hidden"><?php bloginfo( 'name' ); ?></span>
+        </a>
+
+        <a href="https://twitter.com/alexed93?lang=en" class="icon icon--xlarge icon--twitter | social__icon animation--bounce">
+            <span class="is-hidden">Twitter</span>
+        </a>
+
+        <a href="https://twitter.com/alexed93?lang=en" class="icon icon--xlarge icon--facebook | social__icon animation--bounce">
+            <span class="is-hidden">Facebook</span>
+        </a>
+
+        <a href="https://twitter.com/alexed93?lang=en" class="icon icon--xlarge icon--instagram | social__icon animation--bounce">
+            <span class="is-hidden">Instagram</span>
+        </a>
+
+        <div class="cf u-clear u-pad-top">
+            <p class="zeta copyright">Copyright &copy; Stash</p>
+
+            <p class="zeta created-by">Created by
+                <a href="mailto:alexed93@gmail.com" class="link">Alex Edwards</a>
+            </p>
+        </div>
+
     </div>
 </footer>
+
 
 <?php wp_footer(); ?>
 
