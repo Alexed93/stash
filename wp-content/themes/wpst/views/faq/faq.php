fcc9dca70a483cd5b9b12d698bd61e5237322899
Alex Edwards <alexed93@gmail.com>
2018-03-25 15:35:00 +0100

    Added WP functionality to faq

--- wp-content/themes/wpst/views/faq/faq.php
+++ wp-content/themes/wpst/views/faq/faq.php
@@ -1,45 +1,40 @@
+<?php
+
+/**
+ ***************************************************************************
+ * Partial: FAQ
+ ***************************************************************************
+ *
+ * This partial is used for FAQ area
+ *
+ */
+
+// Get featured project
+$questions = get_field('questions', 8);
+
+?>
+
 <div class="faq">
     <h2 class="alpha sub-title">everything else</h2>
 
-    <div class="accordion">
+    <?php if( have_rows('questions', 8) ): ?>
 
+    <div class="accordion">
+        <?php while ( have_rows('questions', 8) ) : the_row(); ?>
+            <?php
+                $question   = get_sub_field('question');
+                $answer     = get_sub_field('answer');
+            ?>
         <div class="accordion-section js-toggle-accordion">
-            <div class="accordion-section-title">how do I create a new group?</div>
+            <div class="accordion-section-title"><?php echo $question; ?></div>
             <span class="accordion__icon icon icon--open icon--medium"></span>
             <div class="accordion-section-content">
-                <p>Mauris interdum fringilla augue vitae tincidunt. Curabitur vitae tortor id eros euismod ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent nulla mi, rutrum ut feugiat at, vestibulum ut neque? Cras tincidunt enim vel aliquet facilisis. Duis congue ullamcorper vehicula. Proin nunc lacus, semper sit amet elit sit amet, aliquet pulvinar erat. Nunc pretium quis sapien eu rhoncus. Suspendisse ornare gravida mi, et placerat tellus tempor vitae.</p>
+                <p><?php echo $answer; ?></p>
             </div><!--end .accordion-section-content-->
         </div>
-
-        <div class="accordion-section js-toggle-accordion">
-            <div class="accordion-section-title">how do I change the due date of my goal?</div>
-            <div class="accordion-section-content">
-                <p>Mauris interdum fringilla augue vitaekjbkb tincidunt. Curabitur vitae tortor id eros euismod ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent nulla mi, rutrum ut feugiat at, vestibulum ut neque? Cras tincidunt enim vel aliquet facilisis. Duis congue ullamcorper vehicula. Proin nunc lacus, semper sit amet elit sit amet, aliquet pulvinar erat. Nunc pretium quis sapien eu rhoncus. Suspendisse ornare gravida mi, et placerat tellus tempor vitae.</p>
-            </div><!--end .accordion-section-content-->
-        </div>
-
-        <div class="accordion-section js-toggle-accordion">
-            <div class="accordion-section-title">my group members aren’t responding!</div>
-            <div class="accordion-section-content">
-                <p>Mauris interdum fringilla augue vitae tincidunt. Curabitur vitae tortor id eros euismod ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent nulla mi, rutrum ut feugiat at, vestibulum ut neque? Cras tincidunt enim vel aliquet facilisis. Duis congue ullamcorper vehicula. Proin nunc lacus, semper sit amet elit sit amet, aliquet pulvinar erat. Nunc pretium quis sapien eu rhoncus. Suspendisse ornare gravida mi, et placerat tellus tempor vitae.</p>
-            </div><!--end .accordion-section-content-->
-        </div>
-
-        <div class="accordion-section js-toggle-accordion">
-            <div class="accordion-section-title">why is my task still showning as “incomplete”?</div>
-            <div class="accordion-section-content">
-                <p>Mauris interdum fringilla augue vitae tincidunt. Curabitur vitae tortor id eros euismod ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent nulla mi, rutrum ut feugiat at, vestibulum ut neque? Cras tincidunt enim vel aliquet facilisis. Duis congue ullamcorper vehicula. Proin nunc lacus, semper sit amet elit sit amet, aliquet pulvinar erat. Nunc pretium quis sapien eu rhoncus. Suspendisse ornare gravida mi, et placerat tellus tempor vitae.</p>
-            </div><!--end .accordion-section-content-->
-        </div>
-
-        <div class="accordion-section js-toggle-accordion">
-            <div class="accordion-section-title">how do I earn more points?</div>
-            <div class="accordion-section-content">
-                <p>Mauris interdum fringilla augue vitae tincidunt. Curabitur vitae tortor id eros euismod ultrices. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent nulla mi, rutrum ut feugiat at, vestibulum ut neque? Cras tincidunt enim vel aliquet facilisis. Duis congue ullamcorper vehicula. Proin nunc lacus, semper sit amet elit sit amet, aliquet pulvinar erat. Nunc pretium quis sapien eu rhoncus. Suspendisse ornare gravida mi, et placerat tellus tempor vitae.</p>
-            </div><!--end .accordion-section-content-->
-        </div>
-
+        <?php endwhile; ?>
     </div>
+    <?php endif; ?>
 
 </div>
 
