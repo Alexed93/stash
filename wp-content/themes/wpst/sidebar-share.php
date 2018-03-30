1ca05abd241e0c7a701a37c73870b0065d251e68
Alex Edwards <alexed93@gmail.com>
2018-03-30 17:24:07 +0100

    Tweak to sidebar

--- wp-content/themes/wpst/sidebar-share.php
+++ wp-content/themes/wpst/sidebar-share.php
@@ -15,7 +15,7 @@
 
 <aside class="sidebar sidebar--share" role="complementary">
     <article class="sidebar__section">
-        <h2 class="gamma | sidebar__heading">Share this post</h2>
+        <h2 class="gamma | sidebar__heading u-push-bottom/2">Share this post</h2>
         <?php if(function_exists('social_warfare')):
             social_warfare();
         endif; ?>
