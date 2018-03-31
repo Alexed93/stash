50996f444cdd1b5c06a24b8ba291971042554947
Alex Edwards <alexed93@gmail.com>
2018-03-31 15:52:55 +0100

    Better blog next and previous post buttons for tablet

--- wp-content/themes/wpst/views/globals/pagination.php
+++ wp-content/themes/wpst/views/globals/pagination.php
@@ -1,4 +1,4 @@
-<nav class="pagination grid__item grid__item--4-12-bp2">
+<nav class="pagination grid__item grid__item--4-12-bp3">
     <?php if ( function_exists( 'wp_pagenavi' ) ) : ?>
         <?php wp_pagenavi(); ?>
     <?php endif; ?>
