dbd4b76d2854af6f7d5bc9da4dd9e6ebc7a50d40
Alex Edwards <alexed93@gmail.com>
2018-04-01 13:17:06 +0100

    Tweak

--- wp-content/themes/wpst/views/front/features.php
+++ wp-content/themes/wpst/views/front/features.php
@@ -11,7 +11,7 @@
         <div class="grid__item grid__item--6-12-bp2">
             <div class="feature-text__container">
                 <div class="feature__text feature__odd">
-                    <h2 class="u-push-bottom/2">1. Feature name</h2>
+                    <h2 class="u-push-bottom/2">1. Groups</h2>
                     <p class="u-zero-bottom">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Sed posuere consectetur est at lobortis. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
                 </div>
             </div>
@@ -24,7 +24,7 @@
         <div class="grid__item grid__item--6-12-bp2">
             <div class="feature-text__container">
                 <div class="feature__text feature__even">
-                    <h2 class="u-push-bottom/2">2. Feature name</h2>
+                    <h2 class="u-push-bottom/2">2. Tasks</h2>
                     <p class="u-zero-bottom">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Sed posuere consectetur est at lobortis. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
                 </div>
             </div>
@@ -51,8 +51,8 @@
         <div class="grid__item grid__item--6-12-bp2">
             <div class="feature-text__container">
                 <div class="feature__text feature__odd">
-                    <h2 class="u-push-bottom/2">3. Feature name</h2>
-                    <p class="u-zero-bottom">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Sed posuere consectetur est at lobortis. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+                    <h2 class="u-push-bottom/2">3. Goals</h2>
+                    <p class="u-zero-bottom">Think of a goal as your virtual piggy bank. Need to save up for a summer holiday? Want to buy your first car? Create a goal by giving it a name and picture. Then, set a due date and a target amount. Now all that's left to do is start saving!</p>
                 </div>
             </div>
         </div>
