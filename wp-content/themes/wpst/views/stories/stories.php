c3ed57e7586a94360349a15c561b17c1390a4e0c
Alex Edwards <alexed93@gmail.com>
2018-04-01 18:04:56 +0100

    Stories page content

--- wp-content/themes/wpst/views/stories/stories.php
+++ wp-content/themes/wpst/views/stories/stories.php
@@ -3,8 +3,8 @@
     <div class="feature__wrap">
         <div class="grid__item grid__item--6-12-bp2 feature-image__wrap">
             <div class="feature__image">
-                <img class="iphone iphone--feature" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/iphone-light.svg" alt="" title="" />
-                <div class="u-push-bottom profile__image" style="background-image: url(' https://source.unsplash.com/random/300x300 ');"></div>
+                <img class="iphone iphone--feature" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/iphone-light.svg" alt="How Bethan uses Stash" title="How Bethan uses Stash" />
+                <div class="u-push-bottom profile__image" style="background-image: url(' <?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/bethan.jpg ');"></div>
                 <span class="feature__background feature__background--1"></span>
             </div>
         </div>
@@ -12,14 +12,14 @@
         <div class="grid__item grid__item--6-12-bp2">
             <div class="feature-text__container">
                 <div class="feature__text feature__odd">
-                    <h2 class="u-push-bottom/2">1. Emily</h2>
+                    <h2 class="u-push-bottom/2">1. Bethan</h2>
                     <h3 class="delta u-push-bottom/2">
                         age: 19
                         <br>occupation: First year student, unemployed
                         <br>living circumstances: University Halls
                     </h3>
-                    <p class="u-zero-top epsilon">Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                    <p class="u-zero-top u-zero-bottom epsilon">Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec sed odio dui.</p>
+                    <p class="u-zero-top epsilon">Moving out for the first time was scary, especially for Bethan who had never paid a bill bigger than a mobile contract. Being in her first year, she was worried about missing out on a fun opportunity of socialising, to be sure of affording food for the next week.</p>
+                    <p class="u-zero-top u-zero-bottom epsilon">Having an overview of her account and monthly expenditure helped Bethan keep track of how much budget she had left for a given month. Sometimes, she even had a little bit to spare that she could add to her goal of a new laptop.</p>
                 </div>
             </div>
         </div>
@@ -31,22 +31,22 @@
         <div class="grid__item grid__item--6-12-bp2">
             <div class="feature-text__container">
                 <div class="feature__text feature__even">
-                    <h2 class="u-push-bottom/2">2. Joe</h2>
+                    <h2 class="u-push-bottom/2">2. Alex</h2>
                     <h3 class="delta u-push-bottom/2">
                         age: 22
                         <br>occupation: Final year, working part-time
-                        <br>living circumstances: Sharing with 3 others
+                        <br>living circumstances: Sharing with 2 others
                     </h3>
-                    <p class="u-zero-top epsilon">Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                    <p class="u-zero-top u-zero-bottom epsilon">Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec sed odio dui.</p>
+                    <p class="u-zero-top epsilon">The pressures of additional deadlines and the prospect of job hunting meant Alex was inclined to put off any thought towards his finances. Knowing he had a student loan to start paying off as well as owing to the bank of mum and dad, Alex found it hard to even look at his account.</p>
+                    <p class="u-zero-top u-zero-bottom epsilon">Constant encouragment through push notifications, a points based system and realistic rewards provided Alex with just the right incentive to change his ways. Within a few weeks, Alex had grasped a routine of working hard on his dissertation and having breaks with his mates, courtsey of the awards he had earnt by completing tasks.</p>
                 </div>
             </div>
         </div>
 
         <div class="grid__item grid__item--6-12-bp2 feature-image__wrap">
             <div class="feature__image">
-                <img class="iphone iphone--feature" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/iphone-light.svg" alt="" title="" />
-                <div class="u-push-bottom profile__image profile__image--right" style="background-image: url(' https://source.unsplash.com/random/300x300 ');"></div>
+                <img class="iphone iphone--feature" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/iphone-light.svg" alt="How Alex uses Stash" title="How Alex uses Stash" />
+                <div class="u-push-bottom profile__image profile__image--right" style="background-image: url(' <?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/alex-2.jpg ');"></div>
                 <span class="feature__background feature__background--2"></span>
             </div>
         </div>
@@ -57,8 +57,8 @@
     <div class="feature__wrap">
         <div class="grid__item grid__item--6-12-bp2 feature-image__wrap">
             <div class="feature__image">
-                <img class="iphone iphone--feature" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/iphone-light.svg" alt="" title="" />
-                <div class="u-push-bottom profile__image" style="background-image: url(' https://source.unsplash.com/random/300x300 ');"></div>
+                <img class="iphone iphone--feature" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/iphone-light.svg" alt="How Ciara uses Stash" title="How Ciara uses Stash" />
+                <div class="u-push-bottom profile__image" style="background-image: url(' <?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/ciara.jpg ');"></div>
                 <span class="feature__background feature__background--3"></span>
                 <span class=""></span>
             </div>
@@ -67,14 +67,14 @@
         <div class="grid__item grid__item--6-12-bp2">
             <div class="feature-text__container">
                 <div class="feature__text feature__odd">
-                    <h2 class="u-push-bottom/2">3. Mark</h2>
+                    <h2 class="u-push-bottom/2">3. Ciara</h2>
                     <h3 class="delta u-push-bottom/2">
                         age: 24
                         <br>occupation: Recent graduate, unemployed
-                        <br>living circumstances: Living alone
+                        <br>living circumstances: Sharing with 2 others
                     </h3>
-                    <p class="u-zero-top epsilon">Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                    <p class="u-zero-top u-zero-bottom epsilon">Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec sed odio dui.</p>
+                    <p class="u-zero-top epsilon">Ciara quickly found herself in the wilderness of the job market, under constant pressure to get some money coming in. The stress of the unknown and having to keep on top of endless paperwork meant she often had to prioritise tasks - sometimes at the expense of her housemates.</p>
+                    <p class="u-zero-top u-zero-bottom epsilon">Once a Lead Tennant was chosen, the group were able to put any differences aside. Keeping track of who had paid their share of the house bills became increasingly easier. The ability to send the occasional reminder to each other meant tempers were kept at bay and the group could enjoy each others company instead.</p>
                 </div>
             </div>
         </div>
