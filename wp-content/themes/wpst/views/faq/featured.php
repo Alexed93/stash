20122c1d5fa0cac2c7b6b57b48d6a8a26c127d05
Alex Edwards <alexed93@gmail.com>
2018-04-01 18:14:49 +0100

    Added featured FAQ answers

--- wp-content/themes/wpst/views/faq/featured.php
+++ wp-content/themes/wpst/views/faq/featured.php
@@ -6,22 +6,22 @@
 
         <div class="grid__item grid__item--4-12-bp2">
             <div class="card card--purple">
-                <h3 class="card__headline u-push-bottom/2">what devices does the app support?</h3>
-                <p class="card__excerpt u-zero-bottom">Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas.</p>
+                <h3 class="card__headline u-push-bottom/2">which devices does the app support?</h3>
+                <p class="card__excerpt u-zero-bottom">Stash currently only supports Apple iOS devices at version 10 and above. We have plans to launch on Android soon though, so keep an eye on our blog for the latest updagtes!.</p>
             </div>
         </div>
 
         <div class="grid__item grid__item--4-12-bp2">
             <div class="card card--red">
                 <h3 class="card__headline u-push-bottom/2">I lost my reward activation email, can I get it back?</h3>
-                <p class="card__excerpt u-zero-bottom">Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Curabitur blandit tempus porttitor.</p>
+                <p class="card__excerpt u-zero-bottom">Sure! Simply open the specific reward and tap "re-send activation email". Reward codes are one time use only, so if you've already used the reward and forgotten, don't expect a new code!</p>
             </div>
         </div>
 
         <div class="grid__item grid__item--4-12-bp2">
             <div class="card card--orange">
                 <h3 class="card__headline u-push-bottom/2">how do I adjust my monthly budget?</h3>
-                <p class="card__excerpt u-zero-bottom">Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                <p class="card__excerpt u-zero-bottom">Simply tap on the cog in the upper right of your screen to open the settings menu. Navigate to "My wallet". From here, you can update your monthly budget or alter your PayPal account settings.</p>
             </div>
         </div>
 
