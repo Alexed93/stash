d4cc61f590e1a01260b54406ae6c73687a233b19
Alex Edwards <alexed93@gmail.com>
2018-04-01 19:00:05 +0100

    Spacing tweaks at bp2

--- wp-content/themes/wpst/views/faq/featured.php
+++ wp-content/themes/wpst/views/faq/featured.php
@@ -4,21 +4,21 @@
 
     <div class="grid grid--flex">
 
-        <div class="grid__item grid__item--4-12-bp2">
+        <div class="grid__item grid__item--4-12-bp3">
             <div class="card card--purple">
                 <h3 class="card__headline u-push-bottom/2">which devices does the app support?</h3>
                 <p class="card__excerpt u-zero-bottom">Stash currently only supports Apple iOS devices at version 10 and above. We have plans to launch on Android soon though, so keep an eye on our blog for the latest updagtes!.</p>
             </div>
         </div>
 
-        <div class="grid__item grid__item--4-12-bp2">
+        <div class="grid__item grid__item--4-12-bp3">
             <div class="card card--red">
                 <h3 class="card__headline u-push-bottom/2">I lost my reward activation email, can I get it back?</h3>
                 <p class="card__excerpt u-zero-bottom">Sure! Simply open the specific reward and tap "re-send activation email". Reward codes are one time use only, so if you've already used the reward and forgotten, don't expect a new code!</p>
             </div>
         </div>
 
-        <div class="grid__item grid__item--4-12-bp2">
+        <div class="grid__item grid__item--4-12-bp3">
             <div class="card card--orange">
                 <h3 class="card__headline u-push-bottom/2">how do I adjust my monthly budget?</h3>
                 <p class="card__excerpt u-zero-bottom">Simply tap on the cog in the upper right of your screen to open the settings menu. Navigate to "My wallet". From here, you can update your monthly budget or alter your PayPal account settings.</p>
